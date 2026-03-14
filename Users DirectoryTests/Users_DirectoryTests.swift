b286baf345de82aaac9df53f0ba0c718cfdb1a5e
Ajay <ajay.gadwal@etmoney.com>
2026-03-14 15:04:22 +0530

    Initial Commit

---
+++ Users DirectoryTests/Users_DirectoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Users_DirectoryTests.swift
+//  Users DirectoryTests
+//
+//  Created by Ajay Gadwal on 14/03/26.
+//
+
+import Testing
+@testable import Users_Directory
+
+struct Users_DirectoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Users DirectoryUITests/Users_DirectoryUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Users_DirectoryUITestsLaunchTests.swift
+//  Users DirectoryUITests
+//
+//  Created by Ajay Gadwal on 14/03/26.
+//
+
+import XCTest
+
+final class Users_DirectoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
